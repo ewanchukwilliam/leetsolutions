e79da7b70f38cb2e03222207b14dc855fdb414d3
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-08-21 16:41:13 -0600

    added a way for me to use the structures methods in the lsp configuration

--- 206.reverse-linked-list.cpp
+++ 206.reverse-linked-list.cpp
@@ -1,18 +1,25 @@
 // @leet start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+  /* Definition for singly-linked list. */
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        
+  ListNode *reverseList(ListNode *head) {
+    ListNode *previous = nullptr;
+    ListNode *next = nullptr;
+
+    while (head != nullptr) {
+      next = head->next;
+      head->next = previous;
+      previous = head;
+      head = next;
     }
+    return previous;
+  }
 };
 // @leet end
