dd5121aa7cfaf052d9f42de07ae36909cb72baa1
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-08-22 09:42:47 -0600

    added reordered list to solution set

--- 143.reorder-list.js
+++ 143.reorder-list.js
@@ -1,4 +1,3 @@
-// @leet start
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -10,22 +9,9 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-	if (!head || !head.next) return;
-	let slow = head,
-		fast = head;
-	while (fast.next && fast.next.next) {
-		slow = slow.next;
-		fast = fast.next.next;
-	}
-	let second = slow.next;
-	slow.next = null; // Split the list into two halves
-	let reversed = reverseList(second);
-	combinelinked(head, reversed);
-};
-var reverseList = function(head) {
-	let previous = null;
-	let next = null;
+var reverse = function(head) {
+	var previous = null;
+	var next = null;
 	while (head) {
 		next = head.next;
 		head.next = previous;
@@ -34,15 +20,24 @@ var reverseList = function(head) {
 	}
 	return previous;
 };
-var combinelinked = function(list1, list2) {
-	while (list1 && list2) {
-		let temp1 = list1.next;
-		let temp2 = list2.next;
-		list1.next = list2;
-		list2.next = temp1; // Ensure the original end of the list1 half connects correctly
-		list1 = temp1;
-		list2 = temp2;
+var reorderList = function(head) {
+	var slow = head;
+	var fast = head;
+	while (fast && fast.next !== null) {
+		slow = slow.next;
+		fast = fast.next.next;
 	}
+	var rev = slow.next;
+	slow.next = null;
+	rev = reverse(rev);
+	var temp = head;
+	while (temp !== null && rev !== null) {
+		var tempNext = temp.next;
+		var revNext = rev.next;
+		temp.next = rev;
+		rev.next = tempNext;
+		temp = tempNext;
+		rev = revNext;
+	}
+	return head;
 };
-
-// @leet end
