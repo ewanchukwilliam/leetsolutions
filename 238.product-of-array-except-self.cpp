940d8ce92849eb0215489f7cdbb92d1997bb026d
wewanchu <wewanchu@ualberta.ca>
2024-09-08 20:23:31 -0600

    added product of array solution of everything except self solution in cpp

--- 238.product-of-array-except-self.cpp
+++ 238.product-of-array-except-self.cpp
@@ -1,22 +1,8 @@
 // @leet start
-#include <vector>
-using namespace std;
 class Solution {
 public:
-  vector<int> productExceptSelf(vector<int> &nums) {
-    int size = nums.size();
-    vector<int> answer(size, 1);
-    int left = 1;
-    for (int i = 0; i < size; i++) {
-      answer[i] = left;
-      left *= nums[i];
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
     }
-    int right = 1;
-    for (int i = size - 1; i >= 0; i--) {
-      answer[i] *= right;
-      right *= nums[i];
-    }
-    return answer;
-  }
 };
 // @leet end
