82024cce94d2e8cdd580c03d2de93de3d6aee564
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-09-09 13:41:27 -0600

    added test to fetching origin

--- 238.product-of-array-except-self.py
+++ 238.product-of-array-except-self.py
@@ -3,7 +3,9 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         length = len(nums)
         answer = [1] * length
+        this is a test for fetching origin main
         multiple = 1
+        this is the pc version
         for i in range(length):
             answer[i] = multiple
             multiple *= nums[i]
