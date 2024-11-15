c9db2a2d9eaba2782e40959f0efbe918546811c5
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-11-15 04:27:21 -0700

    solution in c

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -11,8 +11,23 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *head = new ListNode();
+    ListNode *cur = head;
+    int carry = 0;
+    for (; l1 != nullptr || l2 != nullptr;
+         l1 = l1 == nullptr ? nullptr : l1->next,
+         l2 = l2 == nullptr ? nullptr : l2->next) {
+      int sum =
+          (l1 == nullptr ? 0 : l1->val) + (l2 == nullptr ? 0 : l2->val) + carry;
+      carry = sum / 10;
+      cur->next = new ListNode(sum % 10);
+      cur = cur->next;
     }
+    if (carry > 0) {
+      cur->next = new ListNode(carry);
+    }
+    return head->next;
+  }
 };
 // @leet end
