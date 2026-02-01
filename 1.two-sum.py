a297c182dfce1bdacc757a08dfec62b23fccd216
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-01 15:10:47 -0700

    simplest approach

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -2,13 +2,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash={}
-        for i,num in enumerate(nums):
-            seek=target-num
-            if seek in hash:
-                return hash[seek], i
-            hash[num]=i
+        for i in range(0,len(nums)):
+            hash[nums[i]]=i
+
+        for i in range(0,len(nums)):
+            val = hash.get(target-nums[i])
+            if val is not None and val is not i:
+                return [val, i]
         return []
-
-
         
 # @leet end
