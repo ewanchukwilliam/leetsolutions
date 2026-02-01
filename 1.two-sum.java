5d47a6ab784292bfe1d8cdd3a84213b02e1b5b02
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-01 16:35:56 -0700

    another java attempt

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -2,19 +2,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
-		for (int i =0; i< nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			map.put(nums[i],i);
 		}
 		for (int i = 0; i < nums.length; i++) {
-			int seek = target- nums[i];
-			if (map.containsKey(seek) && map.get(seek)!=i ){
-				return new int[]{map.get(seek), i};
-				
+			if (map.containsKey(target-nums[i]) && map.get(target-nums[i])!=i) {
+				return new int[]{map.get(target-nums[i]),i};
 			}
-	
 		}
 		return new int[0];
-
     }
 }
 // @leet end
