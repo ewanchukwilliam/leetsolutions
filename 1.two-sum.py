e5d83581dc07a0d64607c4204fa9adcbf6eb5081
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-01-31 23:09:00 -0700

    testing new commit pattern

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,12 +1,12 @@
 # @leet start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            if nums[i] in hashmap:
-                return [hashmap[nums[i]], i]
-            else:
-                hashmap[target - nums[i]] = i
-        return []
+        map={}
+        for i in range(0,len(nums)):
+            for n in range(i+1,len(nums)):
+                if nums[i]+nums[n]==target:
+                    return[i,n]
+        return[]
+
         
 # @leet end
