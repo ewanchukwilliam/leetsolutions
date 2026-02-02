633dda880b0eca4244cbb117401ed494749d923c
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-02 00:39:11 -0700

    oops solved quickly

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,15 @@
 // @leet start
 class Solution {
     public int maxProfit(int[] prices) {
-		int l=0;        
-		int r=0;        
-		int answer=0;
-		while (r<prices.length) {
-			if (prices[l]<prices[r]) {
-				answer = Math.max(answer, prices[r]-prices[l]);
-			}else{
-				l=r;
-			}
+		int l = 0;
+		int r = 0;
+		int sol =0;
+		while (l < prices.length && r< prices.length) {
+			if (prices[l]>prices[r]) l=r;
+			if(prices[r]-prices[l] > sol) sol= prices[r]-prices[l];
 			r++;
 		}
-		return answer;
+		return sol;
     }
 }
 // @leet end
