bbb982213fe8c90f0f7ce59f1c518faa03744b13
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-02-03 16:11:12 -0700

    solved once again

--- 125.valid-palindrome.ts
+++ 125.valid-palindrome.ts
@@ -1,10 +1,10 @@
 // @leet start
 function isPalindrome(s: string): boolean {
-	s=s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+	s = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
 	let l = 0;
 	let r = s.length - 1;
 	while (l < r) {
-		if(s.charAt(l)!== s.charAt(r)) return false;
+		if (s.charAt(l) !== s.charAt(r)) return false;
 		l++;
 		r--;
 	}
